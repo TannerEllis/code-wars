6eb8322ef215d1f020cf1751b734f294eab2fb00
MyNameMatters <ellistanner.3@gmail.com>
2022-03-18 14:05:20 -0600

    filter-out-string

--- 8kyu/sum-of-positive.js
+++ 8kyu/sum-of-positive.js
@@ -15,3 +15,14 @@ function positiveSum(arr) {
     return newSum
 }
 
+// I saw this solution and liked the non prototype so wanted to save
+function positiveSum(arr) {
+    var total = 0;    
+    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+      if (arr[i] > 0) {                   // if arr[i] is greater than zero
+        total += arr[i];                  // add arr[i] to total
+      }
+    }
+    return total;                         // return total
+  }
+
